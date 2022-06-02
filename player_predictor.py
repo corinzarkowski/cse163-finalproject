455a9333749856047c166e83f179771836ab7618
Corey Zarkowski <coreyz206@gmail.com>
2022-06-01 20:43:05 -0700

    initial commit

--- player_predictor.py
+++ player_predictor.py
@@ -3,3 +3,11 @@ import requests
 import re
 import pandas as pd
 
+# if os.argument == -refresh-data or datafiles not exist:
+#     init_data()
+
+# check validity of college player
+# if not valid throw error
+
+# train random forest on data files
+# return predictions for player
