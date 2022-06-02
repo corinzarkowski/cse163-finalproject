9c74ad271bcf8d5632c87d29ac918063b182356d
Corey Zarkowski <coreyz206@gmail.com>
2022-06-02 15:17:57 -0700

    added args

--- player_predictor.py
+++ player_predictor.py
@@ -1,13 +1,36 @@
-import os
+import sys
 import requests
 import re
 import pandas as pd
+import argparse
 
-# if os.argument == -refresh-data or datafiles not exist:
-#     init_data()
+def process_args():
+  # TODO: add checks to make sure args are valid
+
+  parser = argparse.ArgumentParser(description='Take college basketball players and return career predictions')
+  parser.add_argument('players', metavar='P', type=str, nargs='+',
+                      help='player(s) to predict')
+  parser.add_argument('--refresh-data', dest='do_refresh', action='store_const',
+                      const=True, default=False,
+                      help='Option to refresh player data from https://www.sports-reference.com/cbb/, https://www.basketball-reference.com/')
+
+  args = parser.parse_args()
+
+  return args.do_refresh, args.players
+
 
 # check validity of college player
 # if not valid throw error
 
 # train random forest on data files
 # return predictions for player
+
+def main():
+  refresh, players = process_args()
+
+  print(refresh)
+  print(players)
+
+
+if __name__ == '__main__':
+  main()
